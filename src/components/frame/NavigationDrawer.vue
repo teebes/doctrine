b53a36a38dcb9e66748fddc6ee10197d2d38d1e1
Thibaud Morel <teebes@gmail.com>
2020-04-13 12:25:58 -0400

    Pluralizing doors on nav bar

--- src/components/frame/NavigationDrawer.vue
+++ src/components/frame/NavigationDrawer.vue
@@ -125,7 +125,7 @@ export default {
         route: "/building/mobs/reactions"
       },
       {
-        label: "Door",
+        label: "Doors",
         route: "/building/doors"
       },
       {
