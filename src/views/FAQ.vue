78fb488385a2055a6e84ae391ea00dbf62821b17
Asmo03935 <Cyberhunter4296@gmail.com>
2025-03-10 16:24:06 +0000

    Update FAQ.vue

--- src/views/FAQ.vue
+++ src/views/FAQ.vue
@@ -52,7 +52,7 @@ export default {
         {
           question: "Is Written Realms free?",
           answer:
-            "Yes! You can play and build worlds free of cost and without any paywalls. However every donation on Written Realms' <a href='https://patreon.com/writtenrealms'>Patreon</a> is highly appreciated. Donations go toward server costs, and beyond that, improving the platform's performance and reach"
+            "Yes! You can play and build worlds free of cost and without any paywalls. However every donation on Written Realms' <a href='https://patreon.com/writtenrealms'>Patreon</a> is highly appreciated. Donations go toward server costs, and beyond that, improving the platform's performance and reach."
         }
       ],
       playing: [
@@ -60,7 +60,7 @@ export default {
           question:
             "I could only start as a warrior.  How do I pick other classes?",
           answer:
-            "Playing through the tutorial on the homepage will set your class as a warrior, and you'll be placed into the flagship world, Edeus, upon completion of the tutorial.  To create a character of another class, visit the Worlds link and create a new character in the lobby of your chosen world and select a different class from the class menu"
+            "Playing through the tutorial on the homepage will set your class as a warrior, and you'll be placed into the flagship world, Edeus, upon completion of the tutorial.  To create a character of another class, visit the Worlds link and create a new character in the lobby of your chosen world and select a different class from the class menu."
         },
         {
           question: "I found a bug!  How do I report it?",
