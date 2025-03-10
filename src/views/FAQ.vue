a31d55a40a31f326ee4bf9752c7d1b8945cb9905
Asmo03935 <Cyberhunter4296@gmail.com>
2025-03-10 16:20:29 +0000

    Update FAQ.vue
    Updated answer to 'how to create multiplayer worlds' and added Q&A 'Is Written Realms free?'

--- src/views/FAQ.vue
+++ src/views/FAQ.vue
@@ -48,6 +48,11 @@ export default {
           question: "Can I connect with my own MUD client through telnet?",
           answer:
             "At this time, clients can only connect through websockets.  While anyone can build their own websocket client to connect to the game, or fork the existing client, players primarily connect through the client found on WrittenRealms.com"
+        },
+        {
+          question: "Is Written Realms free?",
+          answer:
+            "Yes! You can play and build worlds free of cost and without any paywalls. However every donation on Written Realms' <a href='https://patreon.com/writtenrealms'>Patreon</a> is highly appreciated. Donations go toward server costs, and beyond that, improving the platform's performance and reach"
         }
       ],
       playing: [
@@ -55,19 +60,19 @@ export default {
           question:
             "I could only start as a warrior.  How do I pick other classes?",
           answer:
-            "Playing through the tutorial on the homepage will set your class as a warrior, and you'll be placed into the flagship world The Unbroken World upon completion of the tutorial.  To create a character of another class, visit the Worlds link and create a new character in the lobby of your chosen world."
+            "Playing through the tutorial on the homepage will set your class as a warrior, and you'll be placed into the flagship world, Edeus, upon completion of the tutorial.  To create a character of another class, visit the Worlds link and create a new character in the lobby of your chosen world and select a different class from the class menu"
         },
         {
           question: "I found a bug!  How do I report it?",
           answer:
-            "Good work!  Head to the Written Realms Discord and let the staff know on the #bugs channel.  Screenshots or details on how you found the bug will be very helpful."
+            "Good work!  Head to the Written Realms Discord and let the staff know on the #bugs channel.  Screenshots, timestamps and details on how you found the bug will be very helpful."
         }
       ],
       building: [
         {
           question: "How do I make my world multiplayer?",
           answer:
-            "Multiplayer worlds are currently tied to a tier of a Patreon support.  Visit the Written Realms Patreon for more information."
+            "A new world will be multiplayer by default. To make it singleplayer, uncheck 'Is Multiplayer' when creating a new world."
         }
       ]
     };
